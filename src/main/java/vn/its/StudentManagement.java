f5935c6eb4516197bd6aae1fe18fd5f52ddeb548
tuannt39 <tuannt39@gmail.com>
2018-03-20 22:54:09 +0700

    JdbcTemplate

--- src/main/java/vn/its/StudentManagement.java
+++ src/main/java/vn/its/StudentManagement.java
@@ -2,14 +2,14 @@ package vn.its;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import vn.its.dao.StudentJdbcTemplateDao;
+import vn.its.dao.JdbcTemplateDao;
 import vn.its.model.Student;
 
 public class StudentManagement {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        StudentJdbcTemplateDao dao = context.getBean("studentJdbcTemplateDao", StudentJdbcTemplateDao.class);
-        dao.insertStudent(new Student(2, "tuan2", "ha noi2"));
+        JdbcTemplateDao dao = context.getBean("jdbcTemplateDao", JdbcTemplateDao.class);
+        dao.insertStudent(new Student(3, "tuan3", "ha noi3"));
         System.out.println(dao.getAllStudent());
     }
 }
