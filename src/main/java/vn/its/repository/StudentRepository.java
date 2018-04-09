4636d8e7161a2543917bf245f18b3efe05200a39
tuannt39 <tuannt39@gmail.com>
2018-04-09 14:44:06 +0700

    CrudRepository

--- src/main/java/vn/its/repository/StudentRepository.java
+++ src/main/java/vn/its/repository/StudentRepository.java
@@ -1,12 +1,12 @@
 package vn.its.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import vn.its.model.Student;
 
 import java.util.List;
 
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+public interface StudentRepository extends CrudRepository<Student, Integer> {
     Student findByNameAndLocation(String name, String location);
     Student findByNameOrLocation(String name, String location);
     Student findFirstByOrderByNameAsc();
